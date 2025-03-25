5ede1cef6e056b5eb445515f7b58b55220db42b0
SadhwaniMoomal <sadhwanimoomal987@gmail.com>
2025-03-25 18:22:08 +0500

    data

--- 7-constant.php
+++ 7-constant.php
@@ -1,5 +1,19 @@
 <?php
+// we are not declared this way to const/var $data = "ABC";
+
+// 1 way to declared constact Value
+const data = "Constant Values";
+echo data;
 
 
+// const DATA;
+// const data;
+// const _data;
+// const _1data;
+// const _data1;
+
+// 2 way
+define("FulName", "moomal sadhwani");
+echo FulName;
 
 ?>
---
+++ 8-dataType.php
@@ -0,0 +1,57 @@
+<?php
+// Data types
+
+
+// 1. String
+  echo "<h4> String </h4>";
+
+  $name = "Data type String Php";
+  echo var_dump($name); //check datatype and string digit
+  echo "<hr/>";
+
+
+// 2. Integer
+  echo "<h4> Integer </h4>";
+
+  $num = 100;
+  echo var_dump($num);
+
+  echo "<hr/>";
+
+// 3. float
+   echo "<h4> float </h4>";
+
+   $numFloat = 10.1;
+   echo var_dump($numFloat);
+
+   echo "<hr/>";
+// 4. Boolean
+   echo "<h4> Boolean </h4>";
+
+   $numbool = true;
+   echo var_dump($numbool);
+
+   echo "<hr/>";
+// 5. Null
+   echo "<h4> Null </h4>";
+
+   $emptyNull = null;
+   echo var_dump($emptyNull);
+
+   echo "<hr/>";
+// 6. Array
+   echo "<h4> Array </h4>";
+
+   $arr = ["php" , "course" , 2025];
+   echo var_dump($arr);
+
+   echo "<hr/>";
+// 7. Object(class)
+   echo "<h4> object (class)  </h4>";
+
+  //  $user = new className();
+   echo "<hr/>";
+// 8. Recourse
+   echo "<h4> Resource </h4>";
+   echo "<hr/>";
+?>
